51ee3f0ce6c3593de1562216b0c42828db70f27b
lag369 <lag369@drexel.edu>
2023-06-06 16:46:13 -0400

    edits

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,29 +1,117 @@
 import { gsap } from "gsap";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
-import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
-import { GSDevTools } from "gsap/GSDevTools";
-import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+// import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 
-gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools, MorphSVGPlugin);
+// import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+// import { MotionPathHelper } from "gsap/MotionPathHelper";
+// import { GSDevTools } from "gsap/GSDevTools";
+
+// gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools, MorphSVGPlugin, MotionPathHelper);
+gsap.registerPlugin(MotionPathPlugin);
 
 //INIT
-gsap.set("#tl-shape", {scale:0.25});
 gsap.set("#topleft-path", {autoAlpha:0});
 
 
-function shapeAnimation(){
+function firstAnimation(){
     
-    let tl = gsap.timeline();
-    tl.from("#tl-shape", {duration:0.5, ease:"none"})
-        .to("#tl-shape", {duration:0.5, scale:0.5, ease:"none"})
+    let tl = gsap.timeline()
+    tl.to("#topleft-shape", {scale:0, duration:0.5, ease:"power3"})
 
+    .to("#topleft-shape", {scale:3.5, duration:0.5, ease:"power3"})
+    .to("#topright-shape", {scale:3.5, duration:0.5, ease:"power3"})
+    .to("#bottomleft-shape", {scale:3.5, duration:0.5, ease:"power3"})
+    .to("#bottomright-shape", {scale:3.5, duration:0.5, ease:"power3"})
+
+    .to("#topleft-shape", {scale:4, duration:0.5, ease:"power3"})
+    .to("#topright-shape", {scale:4, duration:0.5, ease:"power3"})
+    .to("#bottomleft-shape", {scale:4, duration:0.5, ease:"power3"})
+    .to("#bottomright-shape", {scale:4, duration:0.5, ease:"power3"})
+
+    .to("#topleft-shape", {scale:3.5, duration:0.5, ease:"power3"})
+    .to("#topright-shape", {scale:3.5, duration:0.5, ease:"power3"})
+    .to("#bottomleft-shape", {scale:3.5, duration:0.5, ease:"power3"})
+    .to("#bottomright-shape", {scale:3.5, duration:0.5, ease:"power3"})
+
+    .to("#topleft-shape", {scale:2.5, duration:0.5, ease:"power3"})
+    .to("#topright-shape", {scale:2.5, duration:0.5, ease:"power3"})
+    .to("#bottomleft-shape", {scale:2.5, duration:0.5, ease:"power3"})
+    .to("#bottomright-shape", {scale:2.5, duration:0.5, ease:"power3"})
+    ;
+
+    return tl;   
+}
+
+function secondAnimation(){
+
+    let tl = gsap.timeline()
+    tl.to("#topleft-shape", {scale:1, delay:9, duration:2, motionPath:{path:"#topleft-path", align:"#topleft-path", autorotate: true }, ease:"power3"})
+    .to("#topright-shape", {scale:1, duration:2, motionPath:{path:"#topright-path", align:"#topright-path", autorotate: true }, ease:"power3"})
+    .to("#bottomleft-shape", {scale:1, duration:2, motionPath:{path:"#bottomleft-path", align:"#bottomleft-path", autorotate: true }, ease:"power3"})
+    .to("#bottomright-shape", {scale:1, duration:2, motionPath:{path:"#bottomright-path", align:"#bottomright-path", autorotate: true }, ease:"power3"})
+    ;
+
+    return tl;
+}
+
+function lettersAnimation(){
+
+    let tl = gsap.timeline()
+    tl.to("#coop-logo", {delay:13})
 }
 
 
 let mainTl = gsap.timeline();
-mainTl.add(shapeAnimation())
+mainTl.add(firstAnimation())
+    .add(secondAnimation())
+    .add(lettersAnimation())
 
 ;
 
+//GSDevTools.create();
 
-GSDevTools.create();
+    // not right maybe usable??
+
+    // .from("#topright-shape", {scale:0, duration:0.25, ease:"none"})
+    // .from("#bottomleft-shape", {scale:0, duration:0.25, ease:"none"})
+    // .from("#bottomright-shape", {scale:0, duration:0.25, ease:"none"})
+
+    // .from("#topleft-shape", {scale:2.5, duration:0.75, ease:"power3"})
+    // .from("#topright-shape", {scale:1, duration:0.75, ease:"power3"})
+
+    // .from("#topleft-shape", {scale:3, duration:0.75, ease:"power3"})
+    // .from("#topright-shape", {scale:2, duration:0.75, ease:"power3"})
+
+    // .from("#topleft-shape", {scale:3.5, duration:0.75, ease:"power3"})
+    // .from("#topright-shape", {scale:2.5, duration:0.75, ease:"power3"})
+    // .from("#bottomleft-shape", {scale:1, duration:0.75, ease:"power3"})
+
+    // .from("#topleft-shape", {scale:3, duration:0.75, ease:"power3"})
+    // .from("#topright-shape", {scale:2.5, duration:0.75, ease:"power3"})
+    // .from("#bottomleft-shape", {scale:2, duration:0.75, ease:"power3"})
+    
+    // .from("#topleft-shape", {scale:2.5, duration:0.75, ease:"power3"})
+    // .from("#topright-shape", {scale:3, duration:0.75, ease:"power3"})
+    // .from("#bottomleft-shape", {scale:2.5, duration:0.75, ease:"power3"})
+    // .from("#bottomright-shape", {scale:1, duration:0.75, ease:"power3"})
+
+    // .from("#topleft-shape", {scale:2.5, duration:0.75, ease:"power3"})
+    // .from("#topright-shape", {scale:3, duration:0.75, ease:"power3"})
+    // .from("#bottomleft-shape", {scale:3, duration:0.75, ease:"power3"})
+    // .from("#bottomright-shape", {scale:2, duration:0.75, ease:"power3"})
+
+    // .from("#topleft-shape", {scale:2.5, duration:0.75, ease:"power3"})
+    // .from("#topright-shape", {scale:2.5, duration:0.75, ease:"power3"})
+    // .from("#bottomleft-shape", {scale:3.5, duration:0.75, ease:"power3"})
+    // .from("#bottomright-shape", {scale:2.5, duration:0.75, ease:"power3"})
+
+    // .from("#bottomleft-shape", {scale:2.5, duration:0.75, ease:"power3"})
+    // .from("#bottomright-shape", {scale:3, duration:0.75, ease:"power3"})
+
+    // .from("#bottomright-shape", {scale:3, duration:0.75, ease:"power3"})
+    // .from("#bottomright-shape", {scale:2.5, duration:0.75, ease:"power3"})
+
+    // .from("#topleft-shape", {scale:2, duration:0.75, ease:"power3"})
+    // .from("#topright-shape", {scale:2, duration:0.75, ease:"power3"})
+    // .from("#bottomleft-shape", {scale:2, duration:0.75, ease:"power3"})
+    // .from("#bottomright-shape", {scale:2, duration:0.75, ease:"power3"})
